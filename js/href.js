92697fbb68cdf8b31081a3c596cc1ffc9b75ae3f
mickel <mickel@gmail.com>
2023-12-08 14:39:59 +0800

    big changes

---
+++ js/href.js
@@ -0,0 +1,12 @@
+function openfbLink() {
+    var url = "https://web.facebook.com/mico.gaudicos.5/";
+    window.location.href = url;
+  }
+ 
+ 
+  function opengitLink() {
+    var url = "https://github.com/RipMickel";
+    window.location.href = url;
+  }
+ 
+  
